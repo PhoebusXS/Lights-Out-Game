db0643d16668a29ed2cf4f98a905caebdb5fc197
Jinglianwu <jinglian_wu@mymail.sutd.edu.sg>
2016-12-04 01:48:21 +0800

    counter
    still keep trying of segment decimal counter display

--- source/seven_segment/work/planAhead/seven_segment/seven_segment.srcs/sources_1/imports/verilog/mojo_top_0.v
+++ source/seven_segment/work/planAhead/seven_segment/seven_segment.srcs/sources_1/imports/verilog/mojo_top_0.v
@@ -7,17 +7,13 @@
 module mojo_top_0 (
     input clk,
     input rst_n,
-    output reg [7:0] led,
-    input cclk,
-    output reg spi_miso,
     input spi_ss,
     input spi_mosi,
     input spi_sck,
-    output reg [3:0] spi_channel,
     input avr_tx,
-    output reg avr_rx,
     input avr_rx_busy,
     output reg [7:0] io_seg,
+    output reg [3:0] io_sel,
     input [4:0] io_button,
     input [23:0] io_dip
   );
@@ -33,26 +29,45 @@ module mojo_top_0 (
     .in(M_reset_cond_in),
     .out(M_reset_cond_out)
   );
+  wire [1-1:0] M_edge_detector_out;
+  reg [1-1:0] M_edge_detector_in;
+  edge_detector_2 edge_detector (
+    .clk(clk),
+    .in(M_edge_detector_in),
+    .out(M_edge_detector_out)
+  );
   wire [7-1:0] M_seg_seg;
   wire [4-1:0] M_seg_sel;
   reg [16-1:0] M_seg_values;
-  multi_seven_seg_2 seg (
+  multi_seven_seg_3 seg (
     .clk(clk),
     .rst(rst),
     .values(M_seg_values),
     .seg(M_seg_seg),
     .sel(M_seg_sel)
   );
+  wire [16-1:0] M_dec_ctr_digits;
+  reg [1-1:0] M_dec_ctr_inc;
+  multi_dec_ctr_4 dec_ctr (
+    .clk(clk),
+    .rst(rst),
+    .inc(M_dec_ctr_inc),
+    .digits(M_dec_ctr_digits)
+  );
+  wire [1-1:0] M_ctr_value;
+  counter_5 ctr (
+    .clk(clk),
+    .rst(rst),
+    .value(M_ctr_value)
+  );
   
   always @* begin
     M_reset_cond_in = ~rst_n;
     rst = M_reset_cond_out;
-    led = {3'h0, io_button};
-    spi_miso = 1'bz;
-    spi_channel = 4'bzzzz;
-    avr_rx = 1'bz;
-    M_seg_values = 16'h8531;
+    M_edge_detector_in = M_ctr_value;
+    M_dec_ctr_inc = M_edge_detector_out;
+    M_seg_values = M_dec_ctr_digits;
     io_seg = ~M_seg_seg;
-    io_seg = ~M_seg_sel;
+    io_sel = ~M_seg_sel;
   end
 endmodule
--- source/seven_segment/work/verilog/decoder_8.v
+++ source/seven_segment/work/verilog/decoder_8.v
@@ -8,7 +8,7 @@
    Parameters:
      WIDTH = DIGIT_BITS
 */
-module decoder_5 (
+module decoder_8 (
     input [1:0] in,
     output reg [3:0] out
   );
